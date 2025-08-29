4c1c8e8cc62d27d01960ad664359b793257a7b06
Tiago Bordin <tiagolemespalhano@gmail.com>
2025-08-28 22:14:18 -0300

    Usando arquivos estáticos para mostrar páginas HTML

---
+++ src/main/java/org/example/JavaGptEstudosApplication.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaGptEstudosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaGptEstudosApplication.class, args);
+    }
+}
